d8225e54eeed159bfd8fb3117cb3ca940e614183
salar vahidi <mohammadsalarvahidi@gmail.com>
2024-04-18 16:57:37 +0330

    game serializer for create and bulk create

--- game/serializers/game.py
+++ game/serializers/game.py
@@ -1,30 +1,30 @@
 from rest_framework import serializers
 
 from game.models import Game, RoundResult
+from game.serializers import RoundResultSerializer
 
 
 class GameSerializer(serializers.ModelSerializer):
+    round_result = RoundResultSerializer(many=True, required=True)
+
     class Meta:
         model = Game
         fields = "__all__"
 
     def to_representation(self, instance):
         from user.serializers import MiniUserSerializer
-        from game.serializers import RoundResultSerializer
 
         res = super().to_representation(instance)
-        res["winner"] = MiniUserSerializer(data=instance.winner).data
-        res["room"] = instance.room.name
-        res["result"] = RoundResultSerializer(
-            data=instance.round_result, many=True
-        ).data
+        res["winner"] = MiniUserSerializer(instance.winner).data
+        res["room"] = instance.room.room_name if instance.room else ""
         return res
 
-    def _result_bulk_create(self, result: list):
+    def _result_bulk_create(self, game: Game, round_result):
         bulk_result = []
-        for data in result:
+        for data in round_result:
             bulk_result.append(
                 RoundResult(
+                    game=game,
                     letter=data["letter"],
                     data=data["data"],
                     round_count=data["round_count"],
@@ -33,7 +33,7 @@ class GameSerializer(serializers.ModelSerializer):
         RoundResult.objects.bulk_create(bulk_result)
 
     def create(self, validated_data):
-        results = validated_data.pop("round_result")
+        round_result = validated_data.pop("round_result", [])
         game = super().create(validated_data)
-        self._result_bulk_create(results)
+        self._result_bulk_create(game, round_result)
         return game
