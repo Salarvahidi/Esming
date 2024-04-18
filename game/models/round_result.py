bd3e46ec65eaa41ba72de0d9b122e9531ff1c95f
salar vahidi <mohammadsalarvahidi@gmail.com>
2024-04-18 17:10:04 +0330

    round_count removed from RoundResult

--- game/models/round_result.py
+++ game/models/round_result.py
@@ -17,9 +17,6 @@ class RoundResult(models.Model):
     )
     letter = models.CharField(max_length=1, verbose_name=_("Letter"))
     data = models.JSONField(default=default_data)
-    round_count = models.PositiveIntegerField(
-        blank=True, null=True, verbose_name=_("round count")
-    )
 
     class Meta:
         verbose_name = _("round result")
