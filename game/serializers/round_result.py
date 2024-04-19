c4a1ad09916132bc4020022cc833c08ba4954801
salar vahidi <mohammadsalarvahidi@gmail.com>
2024-04-19 19:45:32 +0330

    validate for round_result serializer

--- game/serializers/round_result.py
+++ game/serializers/round_result.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from game.models import RoundResult
+from game.helpers.validator import check_values, score
 
 
 class RoundResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = RoundResult
         fields = "__all__"
+
+    def validate(self, attrs):
+        check = check_values(attrs["data"])
+        data = score(attrs["letter"], check, attrs["data"])
+        for each in data:
+            each["total_point"] = sum([val[1] for _, val in each["values"].items()])
+        attrs["data"] = data
+        return super().validate(attrs)
